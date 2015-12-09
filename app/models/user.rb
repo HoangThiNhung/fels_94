2a1c84143186e2d5d97690093386faf038fb7d5b
HoangNhung <hoang.thi.nhung@framgia.com>
2015-12-09 16:25:28 +0700

    task-48210-remember-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  attr_accessor :remember_token
+
   before_save {self.email = email.downcase}
   
   validates :name, presence: true, length: {maximum: 50}
@@ -23,6 +25,24 @@ class User < ActiveRecord::Base
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create string, cost: cost
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new remember_digest.is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209023222) do
+ActiveRecord::Schema.define(version: 20151209085641) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "target",     limit: 4
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20151209023222) do
     t.string   "image",           limit: 255
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.string   "remember_digest", limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
