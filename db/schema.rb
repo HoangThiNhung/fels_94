9119af70bd63aaf9be79685601ab3fddd65914d8
HoangNhung <hoang.thi.nhung@framgia.com>
2015-12-09 10:36:33 +0700

    task-47804-user-create-new-account-and-show-user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208020947) do
+ActiveRecord::Schema.define(version: 20151209023222) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "target",     limit: 4
@@ -73,6 +73,8 @@ ActiveRecord::Schema.define(version: 20151208020947) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   create_table "words", force: :cascade do |t|
     t.string   "content",     limit: 255
