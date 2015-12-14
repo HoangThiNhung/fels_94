a9ac5923ad3d6becacb3ab6f2fdde00c8d07e15b
HoangNhung <hoang.thi.nhung@framgia.com>
2015-12-14 15:30:04 +0700

    task-47866-forget-password

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: t("mail_activate")
   end
 
-  def password_reset
-    @greeting = t "greeting"
-
-    mail to: "to@example.org"
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t("password_reset")
   end
 end
