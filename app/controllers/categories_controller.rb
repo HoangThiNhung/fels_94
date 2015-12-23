e94983d8fd575d0605eb900c649c7eae0586ab5e
HoangNhung <hoang.thi.nhung@framgia.com>
2015-12-23 14:35:07 +0700

    start-lesson

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,7 @@ class CategoriesController < ApplicationController
   
   def index
     @categories = Category.paginate page: params[:page], per_page: 5
+    @lesson = Lesson.new
   end
 
   def show
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,17 @@ class Lesson < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :results, dependent: :destroy
+
+  validates :user_id, presence: true
+  validates :category_id, presence: true
+
+  accepts_nested_attributes_for :results, allow_destroy: true
+
+  before_create :add_question
+
+  private
+  def add_question
+    words = category.words.order("RAND()").limit(10)
+    words.each{|word| results.build word: word}
+  end
 end
